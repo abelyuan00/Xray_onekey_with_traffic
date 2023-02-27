2a82bf6c32c8ec1c05582defe3f22df753ed9a37
abelyuan00 <abelyuan0822@gmail.com>
2023-02-27 21:38:01 +0800

    change to direct for now

--- install.sh
+++ install.sh
@@ -393,7 +393,7 @@ PORT=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].port)
 FLOW=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[1].flow | tr -d '"')
 DOMAIN=$(cat ${domain_tmp_dir}/domain)
 
-print_ok "vless://$UUID@$DOMAIN:$PORT?security=tls&flow=$FLOW#XTLS_wulabing-$DOMAIN"
+print_ok "vless://$UUID@$DOMAIN:$PORT?security=xtls&flow=$FLOW#XTLS_wulabing-$DOMAIN"
 print_ok "请手动重启xray服务使新用户生效"
 
 }
