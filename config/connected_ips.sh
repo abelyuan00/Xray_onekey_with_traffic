f80f0ef7ef26e7c4e908a6598c6beac3f298685d
abelyuan00 <abelyuan0822@gmail.com>
2023-03-24 00:18:29 +0800

    connected ip record shell script

--- config/connected_ips.sh
+++ config/connected_ips.sh
@@ -6,4 +6,4 @@ LOGFILE="/var/log/connected_ips.log"
 echo "Starting IP capture on interface $INTERFACE..."
 echo "Writing unique IPs to $LOGFILE"
 
-tcpdump -i $INTERFACE -n -s 0 -l | awk '{print $3}' | awk -F. '{print $1"."$2"."$3"."$4}' | sort -u >> $LOGFILE
+sudo tcpdump -n -i $INTERFACE src port 80 >> $LOGFILE
